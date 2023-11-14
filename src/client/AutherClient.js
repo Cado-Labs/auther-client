c0c213f06ff7dc1798790aa4872e8276676701e1
tysky <dtysky51@gmail.com>
2023-11-14 14:37:02 +0300

    implement auth with auto refresh tokens

--- src/client/AutherClient.js
+++ src/client/AutherClient.js
@@ -1,3 +1,10 @@
+import { verify, decode } from "../lib/jwt"
+
+// TODO: delete
+// const ONE_SECOND_MS = 1000
+const ONE_MINUTE_MS = 60 * 1000
+const ONE_DAY_MS = 24 * 60 * 60 * 1000
+
 export class AutherClient {
   #location = window.location
 
@@ -6,11 +13,12 @@ export class AutherClient {
   #REFRESH_PATH = "/tokens/refresh"
   #LOGIN_PATH = "/login"
 
-  constructor ({ redirectUri, autherUrl, http, appcode }) {
+  constructor ({ redirectUri, autherUrl, http, appcode, logger }) {
     this.redirectUri = redirectUri
     this.autherUrl = autherUrl
     this.http = http
     this.appcode = appcode
+    this.logger = logger
   }
 
   #buildOauthUrl = () => {
@@ -55,5 +63,87 @@ export class AutherClient {
     }
 
     return this.http({ path: this.#REFRESH_PATH, body: { refreshToken } })
+  }
+
+  refreshByTimer = ({ fetchTokens, saveTokens }) => {
+    const { accessToken } = fetchTokens()
+
+    verify(accessToken)
+
+    const decodedToken = decode(accessToken)
+    const tokenExpDateMs = decodedToken.payload.exp * 1000
+
+    let refreshTimeout = (tokenExpDateMs - new Date()) / 2
+
+    // TODO: delete
+    // if (refreshTimeout < ONE_SECOND_MS) {
+    //   refreshTimeout = ONE_SECOND_MS
+    // }
+
+    if (refreshTimeout < ONE_MINUTE_MS) {
+      refreshTimeout = ONE_MINUTE_MS
+    }
+
+    if (refreshTimeout > ONE_DAY_MS) {
+      refreshTimeout = ONE_DAY_MS
+    }
+
+    const tokenExpDate = new Date(tokenExpDateMs)
+    const refreshDate = new Date(Date.now() + refreshTimeout)
+
+    this.logger.log(`Token will expire at ${(tokenExpDate)} [${tokenExpDate.toUTCString()}]`)
+    this.logger.log(`Token will be refreshed at ${refreshDate} [${refreshDate.toUTCString()}]`)
+
+    setTimeout(() => {
+      try {
+        this.refresh({ fetchTokens, saveTokens })
+      }
+      catch (error) {
+        this.logger.error(
+          `Error during tokens refreshing at ${new Date()} [${new Date().toUTCString()}]`,
+        )
+        throw error
+      }
+    }, refreshTimeout)
+  }
+
+  refresh = async ({ fetchTokens, saveTokens }) => {
+    const currentTime = `${new Date()} [${new Date().toUTCString()}]`
+    const { refreshToken } = fetchTokens()
+
+    const response = await this.updateTokens(refreshToken)
+    const tokens = await response.json()
+
+    saveTokens(tokens)
+
+    this.logger.log(`Access token has been refreshed successfully at ${currentTime}`)
+    this.logger.log(`Refresh token has been refreshed successfully at ${currentTime}`)
+    this.refreshByTimer({ fetchTokens, saveTokens })
+  }
+
+  authWithRefresh = async ({ fetchTokens, saveTokens, immediate }) => {
+    const { refreshToken } = fetchTokens()
+    verify(refreshToken)
+
+    immediate
+      ? await this.refresh({ fetchTokens, saveTokens })
+      : this.refreshByTimer({ fetchTokens, saveTokens })
+  }
+
+  authentication = async ({ fetchTokens, saveTokens }) => {
+    const { accessToken } = fetchTokens()
+    try {
+      verify(accessToken)
+    }
+    catch (err) {
+      // TODO: delete
+      this.logger.error("err: ", err)
+      await this.authWithRefresh({ fetchTokens, saveTokens, immediate: true })
+      return
+    }
+    // TODO: delete
+    this.logger.log("fine 💚💚💚💚")
+
+    return this.authWithRefresh({ fetchTokens, saveTokens, immediate: false })
   }
 }
